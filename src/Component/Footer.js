18e60892d08a81a03203e321dca5df03604b9e54
Laxmikant Sambhare <lsambhare42@gmail.com>
2022-07-06 22:47:11 +0530

    source

---
+++ src/Component/Footer.js
@@ -0,0 +1,16 @@
+import { Box, Stack, Typography } from '@mui/material'
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <Box mt="80px" bgcolor={"#fff3f4"}>
+      <Stack gap={"40px"} alignItems="center" px={"40px"} pt="24px">
+        <Typography variant='h5' pb={"40px"} mt="20px" >
+            by nameLax😅😅😅😅
+        </Typography>
+      </Stack>
+    </Box>
+  )
+}
+
+export default Footer
