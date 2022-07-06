031e2141f0c7e13346b6f306b81ce805346f85b6
Laxmikant Sambhare <lsambhare42@gmail.com>
2022-07-06 22:44:12 +0530

    source

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import { Box } from '@mui/material'
+import React from 'react'
+import "./App.css"
+import {Route, Routes } from 'react-router-dom'
+import Home from './Pages/Home'
+import Navbar from './Component/Navbar'
+import Footer from './Component/Footer'
+import ExcerciseDetail from './Pages/ExcerciseDetail'
+const App = () => {
+  return (
+        <Box width="400px" sx={{ width: {xl: '1488px'}}} m="auto">
+            <Navbar/>
+            <Routes>
+                <Route path='/' element={<Home/>}/>
+                <Route path='/excercise/:id' element={<ExcerciseDetail/>}/>
+            </Routes>
+            <Footer/>
+        </Box>
+  )
+}
+
+export default App
